0f55942b800bfbb0520a542ebc0b392e800f79d1
Pascal Braband <pascal.braband@web.de>
2020-08-14 16:51:53 +0200

    Moved layer initialization, adding layers and layer configuration to setup (so that it's only done on TargetView initialization)

--- NautiCom/Views/TargetView.swift
+++ NautiCom/Views/TargetView.swift
@@ -29,7 +29,16 @@ class TargetView: UIView {
     
     
     // MARK: Layers
-    private var levelLayer = CALayer()
+    private let innerDot = CAShapeLayer()
+    private let innerTargetCircle = CAShapeLayer()
+    private let outerTargetCircle = CAShapeLayer()
+    private let verticalLineUpper = CAShapeLayer()
+    private let verticalLineLower = CAShapeLayer()
+    private let horizontalLineLeft = CAShapeLayer()
+    private let horizontalLineRight = CAShapeLayer()
+    private let levelIndicatorLeft = CAShapeLayer()
+    private let levelIndicatorRight = CAShapeLayer()
+    private let levelLayer = CALayer()
     
     
     var deviceMotion = DeviceMotion()
@@ -58,6 +67,49 @@ class TargetView: UIView {
             self.levelLayer.transform = CATransform3DMakeRotation(rotation, 0.0, 0.0, 1.0)
             CATransaction.commit()
         }
+        
+        // Setup layers
+        
+        // Circles
+        self.layer.addSublayer(innerDot)
+        
+        innerTargetCircle.strokeColor = UIColor.white.cgColor
+        innerTargetCircle.fillColor = UIColor.clear.cgColor
+        innerTargetCircle.lineWidth = targetLineWidth
+        self.layer.addSublayer(innerTargetCircle)
+        
+        outerTargetCircle.strokeColor = UIColor.white.cgColor
+        outerTargetCircle.fillColor = UIColor.clear.cgColor
+        outerTargetCircle.lineWidth = targetLineWidth
+        self.layer.addSublayer(outerTargetCircle)
+        
+        // Lines
+        verticalLineUpper.strokeColor = UIColor.white.cgColor
+        verticalLineUpper.lineWidth = targetLineWidth
+        self.layer.addSublayer(verticalLineUpper)
+        
+        verticalLineLower.strokeColor = UIColor.white.cgColor
+        verticalLineLower.lineWidth = targetLineWidth
+        self.layer.addSublayer(verticalLineLower)
+        
+        horizontalLineLeft.strokeColor = UIColor.white.cgColor
+        horizontalLineLeft.lineWidth = targetLineWidth
+        self.layer.addSublayer(horizontalLineLeft)
+        
+        horizontalLineRight.strokeColor = UIColor.white.cgColor
+        horizontalLineRight.lineWidth = targetLineWidth
+        self.layer.addSublayer(horizontalLineRight)
+        
+        // Level indicator
+        self.layer.addSublayer(levelLayer)
+        
+        levelIndicatorLeft.strokeColor = UIColor.white.cgColor
+        levelIndicatorLeft.lineWidth = targetLineWidth
+        levelLayer.addSublayer(levelIndicatorLeft)
+        
+        levelIndicatorRight.strokeColor = UIColor.white.cgColor
+        levelIndicatorRight.lineWidth = targetLineWidth
+        levelLayer.addSublayer(levelIndicatorRight)
     }
     
 
@@ -65,94 +117,56 @@ class TargetView: UIView {
         let targetCenter = CGPoint(x: rect.width/2, y: rect.height/2 - rect.height/2*centerOffset)
         
         // Circles
-        
-        let innerDot = CAShapeLayer()
         innerDot.fillColor = UIColor.white.cgColor
         let dotSize: CGFloat = 2.0
         let dotFrame = CGRect(x: targetCenter.x - dotSize/2, y: targetCenter.y - dotSize/2, width: dotSize, height: dotSize)
         innerDot.path = UIBezierPath(ovalIn: dotFrame).cgPath
-        self.layer.addSublayer(innerDot)
         
-        let innerTargetCircle = CAShapeLayer()
-        innerTargetCircle.strokeColor = UIColor.white.cgColor
-        innerTargetCircle.fillColor = UIColor.clear.cgColor
-        innerTargetCircle.lineWidth = targetLineWidth
         let innerTargetCircleSize: CGFloat = 40.0
         let innerTargetCircleFrame = CGRect(x: targetCenter.x - innerTargetCircleSize/2, y: targetCenter.y - innerTargetCircleSize/2, width: innerTargetCircleSize, height: innerTargetCircleSize)
         innerTargetCircle.path = UIBezierPath(ovalIn: innerTargetCircleFrame).cgPath
-        self.layer.addSublayer(innerTargetCircle)
         
-        let outerTargetCircle = CAShapeLayer()
-        outerTargetCircle.strokeColor = UIColor.white.cgColor
-        outerTargetCircle.fillColor = UIColor.clear.cgColor
-        outerTargetCircle.lineWidth = targetLineWidth
         let outerTargetCircleSize: CGFloat = 180.0
         let outerTargetCircleFrame = CGRect(x: targetCenter.x - outerTargetCircleSize/2, y: targetCenter.y - outerTargetCircleSize/2, width: outerTargetCircleSize, height: outerTargetCircleSize)
         outerTargetCircle.path = UIBezierPath(ovalIn: outerTargetCircleFrame).cgPath
-        self.layer.addSublayer(outerTargetCircle)
         
         
         // Lines
-        
         let verticalLineUpperPath = UIBezierPath()
         verticalLineUpperPath.move(to: CGPoint(x: rect.width/2, y: 0))
         verticalLineUpperPath.addLine(to: CGPoint(x: rect.width/2, y: targetCenter.y - centerPadding))
-        let verticalLineUpper = CAShapeLayer()
-        verticalLineUpper.strokeColor = UIColor.white.cgColor
-        verticalLineUpper.lineWidth = targetLineWidth
         verticalLineUpper.path = verticalLineUpperPath.cgPath
-        self.layer.addSublayer(verticalLineUpper)
         
         let verticalLineLowerPath = UIBezierPath()
         verticalLineLowerPath.move(to: CGPoint(x: rect.width/2, y: rect.height))
         verticalLineLowerPath.addLine(to: CGPoint(x: rect.width/2, y: targetCenter.y + centerPadding))
-        let verticalLineLower = CAShapeLayer()
-        verticalLineLower.strokeColor = UIColor.white.cgColor
-        verticalLineLower.lineWidth = targetLineWidth
         verticalLineLower.path = verticalLineLowerPath.cgPath
-        self.layer.addSublayer(verticalLineLower)
         
         let horizontalLineLeftPath = UIBezierPath()
         horizontalLineLeftPath.move(to: CGPoint(x: 0, y: targetCenter.y))
         horizontalLineLeftPath.addLine(to: CGPoint(x: targetCenter.x - centerPadding, y: targetCenter.y))
-        let horizontalLineLeft = CAShapeLayer()
-        horizontalLineLeft.strokeColor = UIColor.white.cgColor
-        horizontalLineLeft.lineWidth = targetLineWidth
         horizontalLineLeft.path = horizontalLineLeftPath.cgPath
-        self.layer.addSublayer(horizontalLineLeft)
         
         let horizontalLineRightPath = UIBezierPath()
         horizontalLineRightPath.move(to: CGPoint(x: rect.width, y: targetCenter.y))
         horizontalLineRightPath.addLine(to: CGPoint(x: targetCenter.x + centerPadding, y: targetCenter.y))
-        let horizontalLineRight = CAShapeLayer()
-        horizontalLineRight.strokeColor = UIColor.white.cgColor
-        horizontalLineRight.lineWidth = targetLineWidth
         horizontalLineRight.path = horizontalLineRightPath.cgPath
-        self.layer.addSublayer(horizontalLineRight)
         
         
         // Level indicator
         let levelIndicatorSize: CGFloat = outerTargetCircleSize + 2*(levelIndicatorLength + levelIndicatorPadding)
+        levelLayer.transform = CATransform3DIdentity
         levelLayer.frame = CGRect(x: targetCenter.x - levelIndicatorSize/2, y: targetCenter.y - levelIndicatorSize/2, width: levelIndicatorSize, height: levelIndicatorSize)
-        self.layer.addSublayer(levelLayer)
         
         let levelIndicatorLeftPath = UIBezierPath()
         levelIndicatorLeftPath.move(to: CGPoint(x: 0, y: levelLayer.frame.height/2))
         levelIndicatorLeftPath.addLine(to: CGPoint(x: levelIndicatorLength, y: levelLayer.frame.height/2))
-        let levelIndicatorLeft = CAShapeLayer()
-        levelIndicatorLeft.strokeColor = UIColor.white.cgColor
-        levelIndicatorLeft.lineWidth = targetLineWidth
         levelIndicatorLeft.path = levelIndicatorLeftPath.cgPath
-        levelLayer.addSublayer(levelIndicatorLeft)
         
         let levelIndicatorRightPath = UIBezierPath()
         levelIndicatorRightPath.move(to: CGPoint(x: levelLayer.frame.width, y: levelLayer.frame.height/2))
         levelIndicatorRightPath.addLine(to: CGPoint(x: levelLayer.frame.width - levelIndicatorLength, y: levelLayer.frame.height/2))
-        let levelIndicatorRight = CAShapeLayer()
-        levelIndicatorRight.strokeColor = UIColor.white.cgColor
-        levelIndicatorRight.lineWidth = targetLineWidth
         levelIndicatorRight.path = levelIndicatorRightPath.cgPath
-        levelLayer.addSublayer(levelIndicatorRight)
         
         levelLayer.transform = CATransform3DMakeRotation(CGFloat.pi/4, 0.0, 0.0, 1.0)
         
